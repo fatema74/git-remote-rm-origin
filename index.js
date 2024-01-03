1b10363a33bbce18fea78244b3f1c83e099ff1bd
fatema74 <fatema26721@gmail.com>
2024-01-03 20:00:07 +0600

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PASS);
+
 
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.an9sbnh.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,9 +29,56 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const cocacolaCollection = client.db('cocacolaDB').collection('cocacola');
+
+    app.get('/cocacola', async (req, res) => {
+      const cursor = cocacolaCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+    app.get('/cocacola/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cocacolaCollection.findOne(query);
+      res.send(result);
+    })
+
+
     app.post('/cocacola', async (req, res) => {
       const newCocacola = req.body;
       console.log(newCocacola);
+      const result = await cocacolaCollection.insertOne(newCocacola);
+      res.send(result);
+    })
+
+
+    app.put('/cocacola/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updatedCocacola = req.body;
+      const cocacola = {
+        $set: {
+          name: updatedCocacola.name,
+          quantity: updatedCocacola.quantity,
+          supplier: updatedCocacola.supplier,
+          taste: updatedCocacola.taste,
+          category: updatedCocacola.category,
+          details: updatedCocacola.details,
+          photo: updatedCocacola.photo
+        }
+      }
+      const result = await cocacolaCollection.updateOne(filter, cocacola, options);
+      res.send(result);
+    })
+
+    app.delete('/cocacola/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cocacolaCollection.deleteOne(query);
+      res.send(result);
     })
 
     
