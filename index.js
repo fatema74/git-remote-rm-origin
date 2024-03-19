8ee19e3c066d97e0e081f4e6b1ad1871cc15e5a9
fatema74 <fatema26721@gmail.com>
2024-03-19 15:11:29 +0600

    update

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
+require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.an9sbnh.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   },
 });
 
-// app.get('/', async (req, res) => {
-//   res.send("Hello")
-// })
 
-// async function run() {
-//   client.connect();
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
 
-//     catch (error) {
-//     // res.send()
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
 
-// }
-// run().catch(console.dir);
 
-async function run() {
-client.connect();
+
+
+client.connect((err) => {
+
+  if (err) {
+    console.log(err);
+    return;
+  }
 
   const cocacolaCollection = client.db('cocacolaDB').collection('cocacola');
 
@@ -129,14 +124,41 @@ client.connect();
     const result = await userCollection.deleteOne(query);
     res.send(result);
   });
-}
-run().catch(console.dir);
+})
 
+  
+
+
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     // await client.connect();
+
+//     const cocacolaCollection = client.db('cocacolaDB').collection('cocacola');
+
+//     app.get('/cocacola', async (req, res) => {
+//     const cursor = cocacolaCollection.find();
+//     const result = await cursor.toArray();
+//     res.send(result);
+//   });
+
+//     // Send a ping to confirm a successful connection
+//     // await client.db('admin').command({ ping: 1 });
+//     console.log(
+//       'Pinged your deployment. You successfully connected to MongoDB!'
+//     );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 // Send a ping to confirm a successful connection
 // await client.db('admin').command({ ping: 1 });
-console.log('Pinged your deployment. You successfully connected to MongoDB!');
+// console.log('Pinged your deployment. You successfully connected to MongoDB!');
 
 app.get('/', (req, res) => {
   res.send('Coffee making server is running');
